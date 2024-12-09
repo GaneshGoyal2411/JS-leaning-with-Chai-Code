7f856f2d33a925a79ff740dbc3ca402d24543b5a
GaneshGoyal2411 <ganesh.goyal2411@gmail.com>
2024-12-09 10:54:13 +0000

    Variable learning

---
+++ 01_basics/test2.js
@@ -0,0 +1,15 @@
+const accountId = 44335; // change not allowed
+let accountEmail = "ganesh.goyal2411@gmail.com";
+var accountPassword = "12345";
+accountCIry = "Juipur";
+let accountState;
+
+// accountId = 54321;
+accountEmail = "gg@gmail.com";
+accountPassword = "54321";
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCIry,accountState]);
+
+console.log(accountEmail);
+console.log(accountPassword);
