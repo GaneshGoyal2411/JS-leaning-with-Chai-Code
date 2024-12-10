75c718ffa03a10086eef586eba7288f067a779bc
GaneshGoyal2411 <ganesh.goyal2411@gmail.com>
2024-12-10 14:38:12 +0000

    Operators Comparison

---
+++ 01_basics/test4.js
@@ -0,0 +1,13 @@
+// Comparisons
+// console.log(2<1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);// These 3 are not good practice
+console.log(null>=0);
+
+// ===
+console.log(5==="5");
