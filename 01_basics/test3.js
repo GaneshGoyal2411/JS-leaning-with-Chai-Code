017b50745fec23ab9b16df78cc6a0f9c242848ed
GaneshGoyal2411 <ganesh.goyal2411@gmail.com>
2024-12-10 14:20:05 +0000

    Operators (Increment)

--- 01_basics/test3.js
+++ 01_basics/test3.js
@@ -12,6 +12,35 @@
 "ganesh" => true;
 */
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber , stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber , stringNumber);
+
+// *************Operations*************
+
+let value = 3;
+let nagValue = -value;
+// console.log(newValue);
+
+// console.log(2**2);
+
+let str1 = "Hello";
+let str2 = "Ganesh";
+let str3 = str1+str2;
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //if first is String then all would be in String
+// console.log(1+2+"2"); //if last is String then before that would be treat as actual
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1+num2+num3);
+
+// Operators study link = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
