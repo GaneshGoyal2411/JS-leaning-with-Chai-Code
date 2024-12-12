6f408d4c6ea7ba2f10b7aaa02c23bcde4cd8b5ba
GaneshGoyal2411 <ganesh.goyal2411@gmail.com>
2024-12-12 11:10:08 +0000

    String importances

---
+++ 01_basics/test5_String.js
@@ -0,0 +1,17 @@
+const fullName = "Ganesh";
+const repoCount = 50;
+
+// console.log("Name:"+fullName+"Repo:"+repoCount);
+// console.log(`Name ${fullName} Repo:${repoCount}`);
+
+const gameName = new String('Ganes_h');
+// console.log(gameName.toUpperCase);
+// console.log(gameName[5]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('N'));
+
+const newString = gameName.substring(0,3);
+console.log(newString);
