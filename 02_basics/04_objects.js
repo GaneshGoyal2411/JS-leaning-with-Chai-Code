742c7f7befe0e6efd0cc9bad05445000ddde4bba
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-15 07:11:57 +0000

    object ends & API intro

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,18 +20,18 @@ const regularUser ={
 }
 
 // console.log(regularUser.email.fullName.lastName);
-console.log(`Email: ${regularUser.email}`);
+// console.log(`Email: ${regularUser.email}`);
 // console.log(`FirstName: ${regularUser.fullName.firstName}`);
 // console.log(`LastName: ${regularUser.fullName.lastName}`);
-console.log(`FullName: ${regularUser.fullName.firstName} ${regularUser.fullName.lastName}`);
+// console.log(`FullName: ${regularUser.fullName.firstName} ${regularUser.fullName.lastName}`);
 
 // By Chat GDP
 for (const key in regularUser) {
-    console.log(`${key}:`, regularUser[key]);
+    // console.log(`${key}:`, regularUser[key]);
 }
 
 // For nested properties:
-console.log("Full Name:");
+// console.log("Full Name:");
 for (const key in regularUser.fullName) {
     console.log(`  ${key}: ${regularUser.fullName[key]}`);
 }
@@ -47,4 +47,25 @@ const obj3 = {5:"d",6:"e"}
 
 // ***** Easy ******
 const obj4 = {...obj1,...obj2,...obj3};
-console.log(obj4);
+// console.log(obj4);
+
+
+// D-Structure
+const course = {
+    coursename: "JS Hindi",
+    coursePrice: 999,
+    courseTeacher:"Hitesh"
+}
+
+// console.log(course.courseTeacher);
+
+const {courseTeacher:teacher} = course 
+
+console.log(teacher);
+
+// API Inroduction
+{
+    "name":"Hitesh",
+    "coursename":"JS in Hindi",
+    "price":"Free"
+}
