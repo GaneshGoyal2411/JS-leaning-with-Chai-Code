5d375e87f7e73a2d79bc1c92778dadad28fd0ff9
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-16 15:22:01 +0000

    function ends

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,5 +41,49 @@ function userlogin(userName = "sam"){
 }
 
 // console.log(userlogin("ganesh"))
-console.log(userlogin("ganesh"))
+// console.log(userlogin("ganesh"))
 // userlogin("");
+
+// -----------------------------------------------------------
+// Rest Operator & Spread (...) 
+function calculateCartPrice(val1,...num1){
+    // console.log(num1); OR
+    return num1;
+}
+
+// calculateCartPrice(2) OR
+// console.log(calculateCartPrice(2,2,3,5));
+
+
+// ---------------------------Object + Function---------------------------------
+const user = {
+    userName:"ganesh",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`User name is :${anyObject.userName} and Price is ${anyObject.price}`)
+}
+
+// handleObject(user);
+handleObject({
+    userName:"Gan",
+    price:299
+});
+
+// ------------ Same with Array -------------
+const newArray = [100,200,300]
+
+function findSecArray(getArray){
+    return getArray[0]
+}
+
+console.log(findSecArray(newArray));
+// console.log(findSecArray([200,300,400,600]))
+
+const myArray = [500,1000,1500]
+
+function desireInd(anyInd){
+    console.log(anyInd[2]);
+}
+desireInd([200,400,600]);
