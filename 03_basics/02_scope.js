b0429c75e7ea5cd0189a6310d440c9ddccd435bc
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-17 11:06:05 +0000

    functions ends

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -7,9 +7,39 @@ if(true){
     const b =20;
     // 
     // c =50;
-    console.log(`Inner :${a}`);
+    // console.log(`Inner :${a}`);
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+//----------- Twice Functions ----------------
+function one(){
+    const user ="ganesh";
+    // console.log(user);
+
+    function two(){
+        const website = "YouTube"
+
+        //  console.log(`${user} ${website}`);
+    }
+    
+    two();
+}
+one();
+
+
+// ----------------- Different Syntex ---------------
+// 01
+function addOne(num){
+    return num + 1;
+}
+
+// console.log(addOne(9));
+// 02
+const addTwo = function(num){
+    return num + 2;
+}
+
+// console.log(addTwo(8))
