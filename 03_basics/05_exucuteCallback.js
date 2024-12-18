57eff59d7c3f69e2e2a684c4b02ffaf731d2014a
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-18 14:03:52 +0000

    execute and Call Stack

---
+++ 03_basics/05_exucuteCallback.js
@@ -0,0 +1,13 @@
+let val1 = 10;
+let val2 = 10;
+
+function addnums(){
+    let total = val1 + val2;
+    return total;
+}
+
+// let result1 = addnums(val1,val2);
+// console.log(result1);
+
+let result2 = addnums(5,5);
+console.log(result2);
