6f01bed77f1cd054ca1b4abac235b9688572f8ad
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-19 12:23:45 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+const userImail = [];
+
+if (userImail){
+    console.log("Got user email");
+}
+else{
+    console.log("Don't have email");
+}
+
+// false = 0,-0,BigInt,0n,"",null,undefined,NaN
+
+// True = "0",'false', " ", [], function(){}
+
+if (userImail.length === 0){
+    
+}
