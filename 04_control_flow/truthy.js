dfa9a350909e0fd01adfec2d5c1082ef138f5784
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2024-12-20 10:08:19 +0000

    operators done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -35,11 +35,20 @@ const price = 600;
 // price < 500 ? console.log("budget") : console.log("not affordable");
 
 // Pra 1
-const num1 = 30, num2 = 20;
+// const num1 = 30, num2 = 20;
 
 // num1 > num2 ? console.log(`${num1} is greater than ${num2}`):console.log(`${num2} is greater than ${num1}`);
 
 // Pra 2 - Even OR odd
-const num = 11;
-num %2 == 0 ? console.log(`${num} is even`) : console.log(`${num} is odd`);
+// const num = 11;
+// num %2 == 0 ? console.log(`${num} is even`) : console.log(`${num} is odd`);
+
+// Pra 3 + OR -
+// const num = -5;
+// num == 0 ? console.log("number is 0") : num < 0 ? console.log("num is negative") : console.log("number is positive");
+
+// Pra 4 Smaller number
+const num1 = 26, num2 = 15, num3 = 100;
+// num1 < num2 && num3 ? console.log(`${num1} is the smallest Num`) : num2 < num3 ? console.log(`${num2} is the smallest num`) : console.log(`${num3} is the smallest number`);
+
 
