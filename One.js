eb35d81f596857ea768c3dc77bf0905a77d4f84b
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-01-31 21:13:52 +0530

    Sec project BMI calculator
    Sec project BMI calculator

--- One.js
+++ One.js
@@ -1,23 +1,19 @@
-const buttons = document.querySelectorAll('.button');
-// console.log(buttons);
-const body = document.querySelector("body");
+const form = document.querySelector('form')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
 
-buttons.forEach( function (button){
-    console.log(button);
-    button.addEventListener('click', function(e){
-        console.log(e);
-        console.log(e.target);
-        if(e.target.id==='grey'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id==='white'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id==='blue'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id==='yellow'){
-            body.style.backgroundColor=e.target.id;
-        }
-    })
-} )
+   const height = parseInt(document.querySelector('#height').value)
+   const weight = parseInt(document.querySelector('#weight').value)
+   const result = document.querySelector('#result')
+
+   if(height==='' || height<0 || isNaN(height)){
+    result.innerHTML = `Please give valid height ${height}`;
+   } 
+   else if(weight==='' || weight<0 || isNaN(weight)){
+    result.innerHTML = `Please give valid weight ${weight}`;
+   }
+   else{
+   const bmi = (weight/((height*height/1000)).toFixed(2));
+   result.innerHTML = `<span>${bmi}</span>`;
+   }
+})
