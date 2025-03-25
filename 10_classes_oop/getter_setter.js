46c3ce08bd69246db41c25109c7366f7675f37a8
Ganesh Goyal <ganesh.goyal2411@gmail.com>
2025-03-25 19:04:18 +0000

    getter setter done with object, class, constructor

--- 10_classes_oop/getter_setter.js
+++ 10_classes_oop/getter_setter.js
@@ -3,7 +3,21 @@ class User {
         this.email = email;
         this.password = password
     }
+
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+        this._email = value.toUpperCase();
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value.toUpperCase()
+    }
 }
 
-const hitesh = new User("hitesh@gmail.com", "123");
+const hitesh = new User("hitesh@gmail.com", "acb");
 console.log(hitesh);
